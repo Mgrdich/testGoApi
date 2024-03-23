16e090eb075fb3779d9857c6fa317a92f3ddfe0a
sarkis1231 <sakooghly@gmail.com>
2024-03-24 00:59:46 +0400

    fix context key type issue

--- internal/middlewares/movies.go
+++ internal/middlewares/movies.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	".com/internal/db"
@@ -13,18 +12,31 @@ import (
 	"github.com/google/uuid"
 )
 
+// Define a custom type for the context key
+type movieContextKeyType struct{}
+
+// Implement the String method for the context key type
+func (k *movieContextKeyType) String() string {
+	return "movie"
+}
+
+// Create a global instance of the movie context key
+var movieContextKey = &movieContextKeyType{}
+
+// GetMovieContext retrieves movie information from the context
 func GetMovieCtx(ctx context.Context) *models.Movie {
-	return ctx.Value("movie").(*models.Movie)
+	return ctx.Value(movieContextKey).(*models.Movie)
 }
 
+// SetMovieContext sets movie information in the context
 func SetMovieCtx(ctx context.Context, movie *models.Movie) context.Context {
-	return context.WithValue(ctx, "movie", movie)
+	return context.WithValue(ctx, movieContextKey, movie)
 }
 
-// MovieCtx middleware adds movie information to the request context
+// MovieContextMiddleware adds movie information to the request context
 func MovieCtx(moviesStore db.MoviesStore) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		fn := (func(w http.ResponseWriter, r *http.Request) {
+		fn := func(w http.ResponseWriter, r *http.Request) {
 			movieID := chi.URLParam(r, "id")
 			if movieID == "" {
 				render.Render(w, r, server.ErrorBadRequest)
@@ -45,12 +57,10 @@ func MovieCtx(moviesStore db.MoviesStore) func(next http.Handler) http.Handler {
 			}
 
 			// Set movie information in the request context
-
 			ctx := SetMovieCtx(r.Context(), movie)
-
-			fmt.Println(ctx)
 			next.ServeHTTP(w, r.WithContext(ctx))
-		})
+		}
+
 		return http.HandlerFunc(fn)
 	}
 }
