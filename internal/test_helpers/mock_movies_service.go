6c997d650bfd7f0eac5c9ff8672f3ef0071cb32c
sarkis1231 <sakooghly@gmail.com>
2024-06-01 18:19:28 +0400

    add person and movies services mocks

---
+++ internal/test_helpers/mock_movies_service.go
@@ -0,0 +1,34 @@
+package test_helpers
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"testGoApi/internal/models"
+)
+
+type MockMovieService struct{}
+
+func (*MockMovieService) GetAll(_ context.Context) ([]*models.Movie, error) {
+	return nil, nil
+}
+func (*MockMovieService) Get(_ context.Context, id uuid.UUID) (*models.Movie, error) {
+	return &models.Movie{ID: id}, nil
+}
+func (*MockMovieService) Create(_ context.Context, param models.CreateMovieParam) (*models.Movie, error) {
+	return &models.Movie{
+		Title:       param.Title,
+		Director:    param.Director,
+		TicketPrice: param.TicketPrice,
+	}, nil
+}
+func (*MockMovieService) Delete(_ context.Context, id uuid.UUID) error {
+	return nil
+}
+func (*MockMovieService) Update(_ context.Context, id uuid.UUID, param models.UpdateMovieParam) (*models.Movie, error) {
+	return &models.Movie{
+		ID:          id,
+		Title:       param.Title,
+		Director:    param.Director,
+		TicketPrice: param.TicketPrice,
+	}, nil
+}
---
+++ internal/test_helpers/mock_person_service.go
@@ -0,0 +1,38 @@
+package test_helpers
+
+import (
+	"context"
+	"errors"
+	"github.com/google/uuid"
+	"testGoApi/internal/models"
+	"time"
+)
+
+type MockPersonService struct{}
+
+var person = &models.Person{
+	ID:        uuid.New(),
+	FirstName: "Test",
+	LastName:  "Test",
+	CreatedAt: time.Now(),
+}
+
+func (m *MockPersonService) GetAll(_ context.Context) ([]*models.Person, error) {
+	return []*models.Person{
+		person,
+	}, nil
+}
+func (m *MockPersonService) Get(_ context.Context, id uuid.UUID) (*models.Person, error) {
+	if id != person.ID {
+		return nil, errors.New("person not found")
+	}
+	return person, nil
+}
+func (m *MockPersonService) Create(_ context.Context, param models.CreatePerson) (*models.Person, error) {
+	return &models.Person{
+		ID:        uuid.New(),
+		FirstName: param.FirstName,
+		LastName:  param.LastName,
+		CreatedAt: time.Now(),
+	}, nil
+}
