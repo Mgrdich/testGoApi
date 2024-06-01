3fb0370466c036e32c46deaa6c4641db77c3b4c4
sarkis1231 <sakooghly@gmail.com>
2024-06-01 18:39:56 +0400

    add routes test

--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -1,12 +1,98 @@
 package routes
 
-import "testing"
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"net/http/httptest"
+	"testGoApi/configs"
+	"testGoApi/internal/server"
+	"testGoApi/internal/test_helpers"
+	"testing"
+)
 
-// TODO this needs to be discussed
-// whether we need the service from outside or not
+type endpointsTestCase struct {
+	name  string
+	input string
+}
 
-// TestAddRoutes should test all the api endpoints that we have
-// like a minor swagger check
 func TestAddRoutes(t *testing.T) {
+	configs.SetAppConfig(&configs.AppConfig{
+		Environment: "dev",
+	})
 
+	s := &server.Server{
+		Router: chi.NewRouter(),
+	}
+	services := &ApplicationServices{
+		MovieService:  &test_helpers.MockMovieService{}, // Use mock services for testing
+		PersonService: &test_helpers.MockPersonService{},
+	}
+
+	// Call the AddRoutes function
+	AddRoutes(s, services)
+
+	endpointsTestCases := []endpointsTestCase{
+		{name: "Health endpoint", input: "/health"},
+		{name: "Person endpoint", input: "/api/v1/person"},
+		{name: "Movies endpoint", input: "/api/v1/movies"},
+		{name: "Swagger endpoint", input: "/swagger/index.html"},
+	}
+
+	for _, endpoint := range endpointsTestCases {
+		t.Run(endpoint.name, func(t *testing.T) {
+			rr := validateRegisteredRoute(t, s.Router, endpoint.input)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("Route %s not found. Expected status code: %d. Got: %d.", endpoint.input, http.StatusOK, rr.Code)
+			}
+		})
+	}
+}
+
+func TestAddRoutesNonDevEnvironment(t *testing.T) {
+	configs.SetAppConfig(&configs.AppConfig{
+		Environment: "prod",
+	})
+
+	s := &server.Server{
+		Router: chi.NewRouter(),
+	}
+
+	services := &ApplicationServices{
+		MovieService:  &test_helpers.MockMovieService{},
+		PersonService: &test_helpers.MockPersonService{},
+	}
+
+	AddRoutes(s, services)
+
+	endpoint := "/swagger/index.html"
+
+	rr := validateRegisteredRoute(t, s.Router, endpoint)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("Route %s not found. Expected status code: %d. Got: %d.", endpoint, http.StatusNotFound, rr.Code)
+	}
+}
+
+func validateRegisteredRoute(t *testing.T, r *chi.Mux, endpoint string) *httptest.ResponseRecorder {
+	t.Helper()
+
+	// Create a request to the endpoint
+	rr := createNewRequest(t, r, http.MethodGet, endpoint)
+
+	// Check if the response status code indicates the route is registered
+	return rr
+}
+
+func createNewRequest(t *testing.T, r *chi.Mux, method string, endpoint string) *httptest.ResponseRecorder {
+	t.Helper()
+	// Create a request to the endpoint
+	req := httptest.NewRequest(method, endpoint, nil)
+
+	// Create a test response recorder
+	rr := httptest.NewRecorder()
+
+	// Serve the request using the router
+	r.ServeHTTP(rr, req)
+
+	return rr
 }
