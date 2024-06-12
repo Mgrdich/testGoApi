6837bf2f15ff2a731d7dfeac40508dc6bee66a93
sarkis1231 <sakooghly@gmail.com>
2024-06-12 14:48:16 +0400

    update movies route

--- internal/routes/movies.go
+++ internal/routes/movies.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"testGoApi/internal/controller"
 	"testGoApi/internal/middlewares"
@@ -10,11 +12,20 @@ import (
 func GetMoviesRouter(movieService services.MovieService) func(router chi.Router) {
 	return func(r chi.Router) {
 		moviesController := controller.NewMoviesController(movieService)
+
+		r.MethodNotAllowed(controller.HandleMethodNotAllowed)
 		r.Get("/", moviesController.HandleGetAllMovies)
 		r.Post("/", moviesController.HandleCreateMovie)
 
 		r.Route("/{id}", func(r chi.Router) {
-			r.Use(middlewares.GetContextIdFunc(movieService.Get, middlewares.SetMovieCtx))
+			r.Use(
+				middlewares.AllowedMethods(
+					http.MethodGet,
+					http.MethodPut,
+					http.MethodDelete,
+				),
+				middlewares.GetContextIdFunc(movieService.Get, middlewares.SetMovieCtx),
+			)
 			r.Get("/", moviesController.HandleGetMovie)
 			r.Put("/", moviesController.HandleUpdateMovie)
 			r.Delete("/", moviesController.HandleDeleteMovie)
