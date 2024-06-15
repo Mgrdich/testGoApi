0a8d076e100e8c6e0059729d00a3267679c22a09
sarkis1231 <sakooghly@gmail.com>
2024-06-15 15:58:08 +0400

    add NotAllowedMethods test

--- internal/middlewares/helpers_test.go
+++ internal/middlewares/helpers_test.go
@@ -10,43 +10,79 @@ import (
 type MethodsTestCase struct {
 	name           string
 	path           string
-	allowedMethods []string
+	methods        []string
 	testMethods    []string
 	expectedStatus int
 }
 
-func TestAllowedMethods(t *testing.T) {
-	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
+var next = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+})
 
-	allowedMethodsTestCases := []MethodsTestCase{
+func TestAllowedMethods(t *testing.T) {
+	testCases := []MethodsTestCase{
 		{
 			name:           "Allowed method GET",
 			path:           "/",
-			allowedMethods: []string{http.MethodGet},
+			methods:        []string{http.MethodGet},
 			testMethods:    []string{http.MethodGet},
 			expectedStatus: http.StatusOK,
 		},
 		{
 			name:           "Allowed methods GET, POST, PUT, DELETE",
 			path:           "/",
-			allowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+			methods:        []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 			testMethods:    []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 			expectedStatus: http.StatusOK,
 		},
 		{
 			name:           "Allowed methods GET RequestMethods POST, PUT, DELETE",
 			path:           "/",
-			allowedMethods: []string{http.MethodGet},
+			methods:        []string{http.MethodGet},
 			testMethods:    []string{http.MethodPost, http.MethodPut, http.MethodDelete},
 			expectedStatus: http.StatusMethodNotAllowed,
 		},
 	}
 
-	for _, testCase := range allowedMethodsTestCases {
+	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			handler := AllowedMethods(testCase.allowedMethods...)(next)
+			handler := AllowedMethods(testCase.methods...)(next)
+
+			for _, method := range testCase.testMethods {
+				req := httptest.NewRequest(method, testCase.path, nil)
+				rr := httptest.NewRecorder()
+				handler.ServeHTTP(rr, req)
+				fmt.Println(method)
+
+				if status := rr.Code; status != testCase.expectedStatus {
+					t.Errorf("handler returned wrong status code: got %v want %v", status, testCase.expectedStatus)
+				}
+			}
+		})
+	}
+}
+
+func TestNotAllowedMethods(t *testing.T) {
+	testCases := []MethodsTestCase{
+		{
+			name:           "Not Allowed Method GET",
+			path:           "/",
+			methods:        []string{http.MethodGet},
+			testMethods:    []string{http.MethodPost, http.MethodPut, http.MethodDelete},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "Not Allowed Method POST, PUT, DELETE",
+			path:           "/",
+			methods:        []string{http.MethodPost, http.MethodPut, http.MethodDelete},
+			testMethods:    []string{http.MethodPost, http.MethodPut, http.MethodDelete},
+			expectedStatus: http.StatusMethodNotAllowed,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			handler := NotAllowedMethods(testCase.methods...)(next)
 
 			for _, method := range testCase.testMethods {
 				req := httptest.NewRequest(method, testCase.path, nil)
